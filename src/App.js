b622e3219a1071b332d7d093c8371a326b31e663
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-11-04 19:07:24 -0500

    :construction: Sets up base UI components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import "babel-polyfill";
-import React, {Component} from "react";
+import React from "react";
 import Box from "mineral-ui/Box";
+import { ThemeProvider } from "mineral-ui/themes";
 
-const App = () => <Box />;
+const App = () => (
+  <ThemeProvider>
+    <>
+      <Box>Header</Box>
+      <Box inline width={1 / 3}>
+        Sidebar
+      </Box>
+      <Box inline>Content</Box>
+    </>
+  </ThemeProvider>
+);
 export default App;
